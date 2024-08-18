7d9efd30d40af364b833ee2908fecc7f59242ae4
jkasaudhan <jitendra.kasaudhan@wellsterhealth.com>
2024-08-18 20:32:10 +0000

    Added express js logic to dynamically get url path and render the page dynamically.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-
+const fs = require('fs');
 const PORT = process.env.PORT || 5001
 
 express()
@@ -8,4 +8,21 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/:url_path', (req, res) => {
+    // Extract the filename from the URL
+    const filename = req.params.url_path;
+
+    // Construct the full file path (assuming files are in a "public" directory)
+    const filePath = path.join(__dirname, 'public', filename);
+
+    // Check if the file exists
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        // If the file doesn't exist, render the "not found" page
+        return res.render('pages/notfound', { title: 'Page Not Found', filename });
+      }
+      return res.render('pages/' + filename);
+    });
+
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
